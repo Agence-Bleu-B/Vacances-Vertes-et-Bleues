f8a6e8db574c4f2545d1065cb50517abe6b452f7
Corentin Monmasson <corentin.monmasson@gmail.com>
2017-05-22 15:30:00 +0200

    refonte css/bootstrap, header

--- application/views/common/footeracc.php
+++ application/views/common/footeracc.php
@@ -1,38 +1,29 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
-<footer>
-	<div class="container">
-		<ul class="liste_footer" >
-			<li>
-				<h2>Site d'annonces de locations de vacances directement auprès des propriétaires et des campings. Un catalogue diffusé à 20000 exemplaires dans 7 pays européens et au Canada.</h2>
-			</li>
-			<li>
-				<a href="<?php echo site_url('compte');?>">Espace annonceur</a>
-				<a href="<?php echo site_url('mentionslegales');?>">Mentions Légales</a>
-				<a href="<?php echo site_url('cgu');?>">Conditions générales d'utilisations</a>
-			</li>
-			<li>
-				<h6>Contactez-nous</h6>
-				<p>
-					contact@vacances-vertes-bleues.fr
-				</p>
-				<p>&copy; Vacances Vertes et Bleues, 2016</p>
-				<a href="http://www.bleu-b.com/" target="blank"class="agence">Création <?php echo my_img('logo_footer_web.jpg','logo_footer_web.jpg');?></a>
-				<p>Carte par © <a href="https://cmap.comersis.com/" class="alignnone" style="display: inline; position: absolute; margin: 0 0 0 5px;">Comersis<a></p>
-			</li>
-		</ul>
-	</div>
-</footer>
-<!-- script -->
-<script
+
+
+	</div> <!-- /container fluid -->
+
+	<!-- script -->
+	<script
 	src="https://code.jquery.com/jquery-3.2.1.min.js"
 	integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
 	crossorigin="anonymous">
-</script>
-<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-<script type="text/javascript" src="<?php echo base_url().'assets/js/cmap/france.js'?>"></script>
-<script type="text/javascript" src="<?php echo base_url().'assets/js/cmap/cmap.js'?>"></script>
-<script type="text/javascript" src="<?php echo base_url().'assets/js/cmap/regions_hover.js'?>"></script>
-</body>
+	</script>
+	<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="<?php echo base_url().'assets/js/cmap/france.js'?>"></script>
+	<script type="text/javascript" src="<?php echo base_url().'assets/js/cmap/cmap.js'?>"></script>
+	<script type="text/javascript" src="<?php echo base_url().'assets/js/cmap/regions_hover.js'?>"></script>
+	
+	<script type="text/javascript">
+	// Utilitaire "boostrap-like" pour une classe '.pull-down' (dispo dans boostrap v4 plus tard) [http://stackoverflow.com/a/14223553]
+
+	//for each element that is classed as 'pull-down', set its margin-top to the difference between its own height and the height of its parent
+	$('.pull-down').each(function() {
+		var $this = $(this);
+		$this.css('margin-top', $this.parent().height() - $this.height())
+	});
+	</script>
+	</body>
 </html>
--- application/views/common/headeracc.php
+++ application/views/common/headeracc.php
@@ -11,69 +11,73 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<meta name="description" content="Site de locations de vacances directement auprès des propriétaires et des campings. Aussi, un des derniers guide papier à être édité en France et en Belgique."/>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta http-equiv="X-UA-Compatible" content="IE=Edge">
-		<!-- CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="<?php echo css_url('reset');?>" />
-    <link rel="stylesheet" type="text/css" href="<?php echo css_url('style');?>" />
-    <link rel="stylesheet" type="text/css" href="<?php echo css_url('mediaqueries');?>" />
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" type="text/css" href="<?php echo css_url('cmap');?>" />
-		<!-- Google Font -->
+	<!-- CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo css_url('cmap');?>" />
+	<link rel="stylesheet" type="text/css" href="<?php echo css_url('style');?>" />
+	<!-- Google Font -->
 	<link href='https://fonts.googleapis.com/css?family=PT+Sans:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
 	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 </head>
-<section id="home">
-    <video autoplay poster="<?php echo base_url().'assets/images/poster.jpg';?>" id="bgVideo" loop style="z-index: 1;">
-        <!--<source src="<?php echo base_url().'assets/videos/VVB_Video.webm';?>" type="video/webm">
-        <source src="<?php echo base_url().'assets/videos/VVB_Video.mp4';?>" type="video/mp4">-->
-    </video>
-    <header style="position: relative;z-index: 100;">
-        <a href="https://www.facebook.com/VacancesVertesEtBleues/" class="fb" target="_blank">
-            <i class="fa fa-facebook" aria-hidden="true"></i>
-        </a>			
-        <a href="<?php echo site_url('compte');?>">Connexion</a>
-    </header>
-    <div class="container" style="position: relative;z-index: 200;">
-        <nav>
-            <a href="#"><h1>Vacances Vertes et Bleues</h1></a>
-            <ul>
-                <li>
-                    <a href="<?php echo site_url('conseils');?>">Nos Conseils <i class="material-icons">keyboard_arrow_down</i></a>
-                    <ul>
-                        <li><a href="<?php echo site_url('conseils/precautions');?>">Précautions</a></li>
-                        <li><a href="<?php echo site_url('conseils/demander');?>">Vous devez demander</a></li>
-                        <li><a href="<?php echo site_url('conseils/reservations');?>">Réservations</a></li>
-                        <li><a href="<?php echo site_url('conseils/demarches');?>">Démarches</a></li>
-                        <li><a href="<?php echo site_url('conseils/recours');?>">Recours</a></li>
-                    </ul>
-                </li>
-                <li><a href="<?php echo site_url('compte');?>">Déposer une annonce</a></li>
-                <li><a href="<?php echo site_url('compte');?>">Mon Compte</a></li>
-            </ul>
-        </nav>
-        <ul id="menu_video" class="row">
-            <li>
-                <a href="#carte" class="col-md-4 col-xs-12">
-                    <i class="material-icons">search</i>
-                    Trouver la destination<br/>
-                    de vos vacances
-                </a>
-            </li>
-            <li>
-                <a href="<?php echo site_url('conseils');?>" class="col-md-4 col-xs-12">
-                    <i class="fa fa-book" aria-hidden="true"></i>
-                    Avant de louer,<br/>nos conseils
-                </a>
-            </li>
-            <li>
-                <a href="<?php echo site_url('compte');?>" class="col-md-4 col-xs-12">
-                    <i class="material-icons">cloud_upload</i>
-                    Espace Annonceurs
-                    <span>
-                        dépôt et modifications d'annonce
-                    </span>
-                </a>
-            </li>
-        </ul>
-    </div>
-</section>
+
+<body> <!-- close in footeracc.php	 -->
+	<div class="container-fluid"> <!-- close in footeracc.php	 -->
+		<div class="row"> <!-- bandeau connexion -->
+			<div class="col-xs-12">
+				<div class="pull-right btn-toolbar">
+					<div class="btn-group">
+						<button class="btn" type="button">Connexion</button>
+					</div>
+					<div class="btn-group">
+						<button class="btn" type="button"><i class="fa fa-facebook-official" aria-hidden="true"></i></button>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<header class="row"> <!-- header video (balise unique page) -->
+			<div class="col-xs-12 col-md-10 col-md-offset-1 col-lg-8 col-lg-offset-2">
+				<div class="row"> <!-- Menu -->
+					<h1 class="col-xs-10 col-xs-offset-1 col-sm-4 col-sm-offset-0 col-md-4 vertical-align-item"> <!-- Logo -->
+						<a href="<?php echo site_url('accueil');?>">
+							<?php echo my_img('logo.png',"Vacances Vertes et Bleues","img-responsive") ?>
+						</a>
+					</h1><!--
+			 --><nav class="hidden-xs col-sm-8 col-sm-offset-0 col-md-7 col-md-offset-1 vertical-align-item"> <!-- links -->
+			 			<a href="#">Nos conseils</a>
+					 	<a href="#">Déposer une annonce</a>
+					 	<a href="#">Mon compte</a>
+					</nav>
+				</div>
+
+				<nav class="row"> <!-- Sub-Menu -->
+					<div class="col-xs-12 col-sm-4">
+						<button class="btn col-xs-12" type="button">
+							<i class="fa fa-search" aria-hidden="true"></i>
+							<br>
+							Trouver la destination <br>de vos vacances
+						</button>
+					</div>
+					<div class="col-xs-12 col-sm-4">
+						<button class="btn col-xs-12" type="button">
+							<i class="fa fa-book" aria-hidden="true"></i>
+							<br>
+							Avant de louer, <br>nos conseils
+						</button>
+					</div>
+					<div class="col-xs-12 col-sm-4">
+						<button class="btn col-xs-12" type="button">
+							<i class="fa fa-cloud-upload" aria-hidden="true"></i>
+							<br>
+							Espace Annonceurs <br><small>Dépôt et modifications d'annonces</small>
+						</button>
+					</div>
+				</nav>
+			</div>
+
+			<video autoplay poster="<?php echo base_url().'assets/images/poster.jpg';?>" id="bgVideo" loop>
+	        <!-- <source src="<?php echo base_url().'assets/videos/VVB_Video.webm';?>" type="video/webm">
+	        <source src="<?php echo base_url().'assets/videos/VVB_Video.mp4';?>" type="video/mp4"> -->
+	    </video>
+		</header> <!-- /header (video) -->
