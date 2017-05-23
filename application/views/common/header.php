38a28d0ff136f81bbb0b9dac4d847d43cc5b685b
Corentin Monmasson <corentin.monmasson@gmail.com>
2017-05-23 18:04:10 +0200

    refonte css, detail

--- application/views/common/header.php
+++ application/views/common/header.php
@@ -6,48 +6,50 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
 
-	<title><?php echo $title; ?></title>
+	<title>Vacances Vertes et Bleues</title>
 
-	<meta name="description" content="<?php echo $description; ?>"/>
+	<meta name="description" content="Site de locations de vacances directement auprès des propriétaires et des campings. Aussi, un des derniers guide papier à être édité en France et en Belgique."/>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta http-equiv="X-UA-Compatible" content="IE=Edge">
-		<!-- CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="<?php echo css_url('reset');?>" />
-    <link rel="stylesheet" type="text/css" href="<?php echo css_url('style');?>" />
-    <link rel="stylesheet" type="text/css" href="<?php echo css_url('mediaqueries');?>" />
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-		<!-- Google Font -->
+	<!-- CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo css_url('cmap');?>" />
+	<link rel="stylesheet" type="text/css" href="<?php echo css_url('style');?>" />
+	<!-- Google Font -->
 	<link href='https://fonts.googleapis.com/css?family=PT+Sans:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
 	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 </head>
-<body>
-    <section id="haut">
-        <header>
-            <a href="https://www.facebook.com/VacancesVertesEtBleues/" class="fb" target="_blank">
-                <i class="fa fa-facebook" aria-hidden="true"></i>
-            </a>
-            <span>
-                <a href="<?php echo site_url('compte');?>" >Connexion</a>
-            </span>
-        </header>
-        <div class="container">
-            <nav>
-                <a href="<?php echo site_url();?>"><h1>Vacances Vertes et Bleues</h1></a>
-                <ul>
-                    <li>
-                        <a href="<?php echo site_url('conseils');?>">Nos Conseils <i class="material-icons">keyboard_arrow_down</i></a>
-                        <ul>
-                            <li><a href="<?php echo site_url('conseils/precautions');?>">Précautions</a></li>
-                            <li><a href="<?php echo site_url('conseils/demander');?>">Vous devez demander</a></li>
-                            <li><a href="<?php echo site_url('conseils/reservations');?>">Réservations</a></li>
-                            <li><a href="<?php echo site_url('conseils/demarches');?>">Démarches</a></li>
-                            <li><a href="<?php echo site_url('conseils/recours');?>">Recours</a></li>
-                        </ul>
-                    </li>
-                    <li><a href="<?php echo site_url('compte');?>">Déposer une annonce</a></li>
-                    <li><a href="<?php echo site_url('compte');?>">Mon Compte</a></li>
-                </ul>
-            </nav>
-        </div>	
-    </section>	
+
+<body> <!-- close in footeracc.php	 -->
+	<div class="container-fluid"> <!-- close in footeracc.php	 -->
+
+		<div id="bandeau" class="row"> <!-- bandeau connexion -->
+			<div class="col-xs-12">
+				<div class="pull-right btn-toolbar">
+					<div class="btn-group">
+						<button class="btn" type="button">Connexion</button>
+					</div>
+					<div class="btn-group">
+						<button class="btn" type="button"><i class="fa fa-facebook-official" aria-hidden="true"></i></button>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<header class="row principal"> <!-- header video (balise unique page) -->
+			<div class="col-xs-12 col-md-10 col-md-offset-1 col-lg-8 col-lg-offset-2">
+				<div class="row"> <!-- Menu -->
+					<h1 class="col-xs-10 col-xs-offset-1 col-sm-4 col-sm-offset-0 col-md-4 vertical-align-item"> <!-- Logo -->
+						<a href="<?php echo site_url('accueil');?>">
+							<?php echo my_img('logo.png',"Vacances Vertes et Bleues","img-responsive") ?>
+						</a>
+					</h1><!--
+			 --><nav class="hidden-xs col-sm-8 col-sm-offset-0 col-md-7 col-md-offset-1 vertical-align-item"> <!-- links -->
+			 			<a href="#">Nos conseils</a>
+					 	<a href="#">Déposer une annonce</a>
+					 	<a href="#">Mon compte</a>
+					</nav>
+				</div>
+			</div>
+		</header> <!-- /header (video) -->
