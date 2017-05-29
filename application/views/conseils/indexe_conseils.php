f93f1d2f771b43431913588c19a8f7d5cc8e9023
Corentin Monmasson <corentin.monmasson@gmail.com>
2017-05-29 10:31:51 +0200

    refonte css conseils (fini)

--- application/views/conseils/indexe_conseils.php
+++ application/views/conseils/indexe_conseils.php
@@ -1,50 +1,44 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
-<section id="page">
-	<section id="content">
-		<div class="container" itemprop="text">
-			<h3 class="nannonce">Nos conseils</h3>
-			<h5>avant de louer, nos conseils</h5>
-			<ul>
-				<li class="conseil_elmt">
-                    <p>
-                        <i class="fa fa-square" aria-hidden="true">
-                        </i>
-                        Avant de réserver un appartement, un gîte une maison ou une chambre d’hôtel, trouvé par le biais des annonces publiées dans cet ouvrage, nous vous recommandons de lire nos conseils. Vous éviterez ainsi toute déception lors de votre arrivée et serez à même de vous défendre en cas de problèmes en cours de location.
-                    </p>
-                    <p>
-                        <i class="fa fa-square" aria-hidden="true">
-                        </i>
-                        <a href="<?php echo site_url('conseils/precautions');?>">Précautions</a>
-                    </p>
-                    <p>
-                        <i class="fa fa-square" aria-hidden="true">
-                        </i>
-                        <a href="<?php echo site_url('conseils/demander');?>">Ce que vous devez demander</a>
-                    </p>
-                    <p>
-                        <i class="fa fa-square" aria-hidden="true">
-                        </i>
-                        <a href="<?php echo site_url('conseils/reservations');?>">Réservations</a>
-                    </p>
-                    <p>
-                        <i class="fa fa-square" aria-hidden="true">
-                        </i>
-                        <a href="<?php echo site_url('conseils/demarches');?>">Démarches</a>
-                    </p>
-                    <p>
-                        <i class="fa fa-square" aria-hidden="true">
-                        </i>
-                        <a href="<?php echo site_url('conseils/recours');?>">Recours</a>
-                    </p>
-                </li>
-                <li class="conseil_elmt">
-					<div class="illust_page">
-						<?php echo my_img('conseils/person-woman-apple-hotel.jpg','person-woman-apple-hotel'); ?>	
-					</div>
-				</li>
-			</ul>
+<section id="content" class="row">
+	<section id="conseils" itemprop="text">
+		<div class="col-xs-12 col-md-10 col-md-offset-1 col-lg-8 col-lg-offset-2">
+			<h3>Nos conseils</h3>
+			<div class="col-xs-12 col-md-6 text-justify vertical-align-item">
+				<strong>Avant de louer, nos conseils</strong>
+				<p>
+					<i class="fa fa-square" aria-hidden="true"></i> Avant de réserver un appartement, un gîte une maison ou une chambre d’hôtel, trouvé par le biais des annonces publiées dans cet ouvrage, nous vous recommandons de lire nos conseils. Vous éviterez ainsi toute déception lors de votre arrivée et serez à même de vous défendre en cas de problèmes en cours de location.
+				</p>
+				<p>
+					<i class="fa fa-square" aria-hidden="true">
+					</i>
+					<a href="<?php echo site_url('conseils/precautions');?>"> Précautions</a>
+				</p>
+				<p>
+					<i class="fa fa-square" aria-hidden="true">
+					</i>
+					<a href="<?php echo site_url('conseils/demander');?>"> Ce que vous devez demander</a>
+				</p>
+				<p>
+					<i class="fa fa-square" aria-hidden="true">
+					</i>
+					<a href="<?php echo site_url('conseils/reservations');?>"> Réservations</a>
+				</p>
+				<p>
+					<i class="fa fa-square" aria-hidden="true">
+					</i>
+					<a href="<?php echo site_url('conseils/demarches');?>"> Démarches</a>
+				</p>
+				<p>
+					<i class="fa fa-square" aria-hidden="true">
+					</i>
+					<a href="<?php echo site_url('conseils/recours');?>"> Recours</a>
+				</p>
+			</div><!--
+	 --><div class="col-xs-8 col-xs-offset-2 col-md-6 col-md-offset-0 vertical-align-item">
+				<?php echo my_img('conseils/person-woman-apple-hotel.jpg','','img-responsive center-block'); ?>
+			</div>
 		</div>
 	</section>
 </section>
