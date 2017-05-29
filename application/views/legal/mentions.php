39c2cca41124b655a8342bf5ebcbfcb94743d1ee
Corentin Monmasson <corentin.monmasson@gmail.com>
2017-05-29 15:13:19 +0200

    legal

--- application/views/legal/mentions.php
+++ application/views/legal/mentions.php
@@ -3,10 +3,36 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 
 <section id="content" class="row"> <!-- content -->
-  <div class="col-xs-12 col-md-10 col-md-offset-1 col-lg-8 col-lg-offset-2">
+  <section id="legal">
+    <div class="col-xs-12 col-md-10 col-md-offset-1 col-lg-8 col-lg-offset-2">
+        <address>
+          <h3>Propriétaire : Vacances Vertes et Bleues</h3>
+          <strong>Adresse : </strong>Parc d'affaires La Billardais - Route de Ploubalay - 22100 Dinan/Taden <br>
+          <strong>Responsable de la publication : </strong>Thierry eustache <br>
+          <strong>Tél. : </strong>02 96 85 80 88 <br>
+          <strong>E-mail : </strong>conctact@vacances-bleues-vertes.fr <br>
+          <strong>Numéro siret : </strong>327241857 <br>
 
-    <div class="row"> <!-- content top -->
+          <h3>Création : Agence Bleu B</h3>
+          <strong>Adresse : </strong>Parc d'affaires La Billardais - Route de Ploubalay - 22100 Dinan/Taden <br>
+          <strong>Tél. : </strong>02 96 85 80 88 - Fax: 02 96 85 16 93 <br>
+          <strong>E-mail : </strong>webmaster@bleu-b.com <br>
+          <strong>Web : </strong>www.bleu-b.com <br>
+
+          <h3>Hébergement : Gandi</h3>
+          <strong>Web : </strong>www.gandi.net <br>
+        </address>
+        <h3>
+          Engagements
+        </h3>
+        <p>
+          Nous nous engageons à être très vigilant sur la fiabilité de l'information mise à la disposition des internautes qui consultent ce site. Nous ne saurions en revanche être tenus pour responsable d'erreurs, d'omissions ou des résultats qui pourraient être obtenus par un usage inapproprié de ces informations. Pour vous permettre de compléter vos recherches, nous pouvons être amené à vous proposer, à travers un lien hypertexte, de consulter un site spécialisé qui nous paraît digne d'intérêt dans un contexte précis. Sans pour autant pouvoir garantir le contrôle des informations délivrées sur le site en question. Propriété intellectuelle
+          Toute représentation et/ou reproduction et/ou exploitation partielle ou totale de ce site, par quelques procédés que se soient, sans l'autorisation expresse et préalable est interdite et constituerait une contrefaçon au sens des articles L 335-2 et suivants du code de la propriété intellectuelle. Données personnelles
+          Nous pouvons collecter certaines informations personnelles permettant directement ou indirectement d'identifier l'utilisateur. Ces informations ne sont en aucune façon divulguées, données ou vendues à quiconque. Nous vous rappelons que vous disposez d’un droit d’accès, de modification, de rectification et de suppression des données qui vous concernent (articles 39 et suivants de la loi du 6 janvier 1978). Pour exercer ces droits, adressez-vous à webmaster@bleu-b.com.
+        </p>
+
     </div>
+  </section>
 
   </div>
 </section>
