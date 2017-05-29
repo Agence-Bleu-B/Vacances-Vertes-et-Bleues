e6b2527d934e9a34719cb7ea018bf8993398cd40
AdrienAlbant <adalbant.adrien@gmail.com>
2017-05-29 14:18:46 +0200

    Update menu_lang.php

--- application/language/english/menu_lang.php
+++ application/language/english/menu_lang.php
@@ -1,11 +1,11 @@
 <?php
 //menu header
-$lang['menu_conseils'] = 'our advices';
-$lang['menu_depot'] = 'déposer une annonce';
-$lang['menu_moncompte'] = 'mon compte';
-$lang['menu_connexion'] = 'connexion';
-$lang['menu_precautions'] = 'précautions';
-$lang['menu_demander'] = "vous devez demander";
-$lang['menu_reservations'] = "réservations" ;
-$lang['menu_demarches'] = 'démarches';
-$lang['menu_recours'] = 'recours';
+$lang['menu_conseils'] = 'Our advices';
+$lang['menu_depot'] = 'Post your ad';
+$lang['menu_moncompte'] = 'My account';
+$lang['menu_connexion'] = 'Connection';
+$lang['menu_precautions'] = 'Caution';
+$lang['menu_demander'] = "You have to ask";
+$lang['menu_reservations'] = "Bookings" ;
+$lang['menu_demarches'] = 'Process';
+$lang['menu_recours'] = 'Appeal';
